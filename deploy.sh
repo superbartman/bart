87af830e04e3e8c9c666c0aba2642d55298e456f
bei.ding <bei.ding@shopee.com>
2024-03-22 11:57:03 +0800

    feat: add

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,7 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-git push origin master
+git push
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
